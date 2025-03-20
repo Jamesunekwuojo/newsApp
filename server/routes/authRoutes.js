146cc5c3f9eb5036fc768a9f1e737531da526f6c
Jamesunekwuojo <unekwuojoabah4@gmail.com>
2025-03-20 10:35:16 +0100

    updated server url

--- server/routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { checkAuth } from "../controller/authController.js";
-
-const router = express.Router()
-
-router.get('/', checkAuth)
-
-
-export default router
