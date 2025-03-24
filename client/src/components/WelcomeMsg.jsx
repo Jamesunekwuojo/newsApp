4345cb01c93c8e9cd800a5bcb27fefca290c2765
Jamesunekwuojo <unekwuojoabah4@gmail.com>
2025-03-24 13:54:15 +0100

    updated title

--- client/src/components/WelcomeMsg.jsx
+++ client/src/components/WelcomeMsg.jsx
@@ -5,20 +5,20 @@ const WelcomeMsg = () => {
     <div className="flex min-h-screen flex-col items-center justify-center bg-blue-50 p-6">
       <div className="flex-row items-center justify-center bg-blue-50 p-6">
         <h2 className="text-center text-gray-800 text-5xl font-bold ">
-          Hello Welcome to Zavod newslist
+          Hello Welcome to User Management Hub
         </h2>
       </div>
 
       <div className="flex flex-row items-center">
         <Link to="/admin">
           <button className="bg-orange-500  text-white font-bold py-2 rounded-lg hover:bg-orange-600 transition cursor-pointer p-4 mx-4 my-4">
-            Create News
+            Create News for users
           </button>
         </Link>
 
         <Link to="/newslist">
           <button className="bg-orange-500 cursor-pointer text-white font-bold py-2 rounded-lg hover:bg-orange-600 transition p-4 mx-4 my-4">
-            View News
+            View News for users
           </button>
         </Link>
       </div>
